59828f986ad1855f68ee11e97ad3be8a362156f5
Chris Wendt <chrismwendt@gmail.com>
2013-11-13 18:32:53 -0500

    Add support for running Haskell problems

--- run_problem.pl
+++ run_problem.pl
@@ -10,6 +10,10 @@ if ($language eq 'java') {
     print `javac main.java`;
     print `java main`;
     print `rm main.class`;
+} elsif ($language eq 'haskell') {
+    print `ghc main.hs`;
+    print `./main`;
+    print `rm main.o main.hi main`;
 }
 
 chdir($cwd) or die "$!";
