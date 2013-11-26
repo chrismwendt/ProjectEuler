812d705001a1e6fb440843138538ec6210210722
Chris Wendt <chrismwendt@gmail.com>
2013-11-26 17:00:12 -0500

    Optimize Problem 7 Java by testing primality only on primes

--- Problem 7/main.java
+++ Problem 7/main.java
@@ -6,14 +6,22 @@ What is the 10 001st prime number?
 
 public class main {
     public static void main(String[] args) {
-        int number = 1, i = 0;
-        outer: while (i < 10001) {
+        int[] primes = new int[10001];
+        primes[0] = 2;
+        int ind = 0;
+
+        int number = 2;
+        outer: for (int i = 1; i < 10001;) {
             number++;
-            for (int j = 2; j <= Math.sqrt(number); j++) {
-                if (number%j == 0) {
+            if (number%2 == 0) {
+                continue outer;
+            }
+            for (int j = 0; primes[j] <= Math.sqrt(number); j++) {
+                if (number%primes[j] == 0) {
                     continue outer;
                 }
             }
+            primes[ind++] = number;
             i++;
         }
         System.out.println(number);
