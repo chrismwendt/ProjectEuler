cb46c2d01d21e313c98c41d7caf8b0658f68fdfb
Chris Wendt <chrismwendt@gmail.com>
2013-11-26 19:05:24 -0500

    Print the value instead of a list in Problem 9

--- Problem 9/main.hs
+++ Problem 9/main.hs
@@ -1,6 +1,6 @@
 import Data.List
 
-main = putStrLn . show $ map product [[a,b,c] | b <- [292..499], let (a, m) = (500000 - 1000 * b) `divMod` (1000 - b), let c = 1000 - a - b, m == 0]
+main = putStrLn . show . head $ map product [[a,b,c] | b <- [292..499], let (a, m) = (500000 - 1000 * b) `divMod` (1000 - b), let c = 1000 - a - b, m == 0]
 
 -- (0) given a < b < c
 -- (1) given a^2 + b^2 = c^2
