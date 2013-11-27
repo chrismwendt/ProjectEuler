06ceda87f2bdaaa65447d681e4c021b6d26f3e89
Chris Wendt <chrismwendt@gmail.com>
2013-11-26 22:06:55 -0500

    Solved Problem 10 in Haskell

---
+++ Problem 10/main.hs
@@ -0,0 +1,8 @@
+main = do
+    print . sum . takeWhile (< 2000000) $ primes
+
+primes :: [Integer]
+primes = 2 : [x | x <- [3,5..], not . any (`divides` x) $ takeWhile (<= sqrt' x) primes]
+    where
+        a `divides` b = b `mod` a == 0
+        sqrt' x = (floor . sqrt . fromIntegral) x
