a5cb659b6988d00c81c728b04f273c9332275ec9
Chris Wendt <chrismwendt@gmail.com>
2013-12-14 21:36:05 -0500

    Solve Problem 24 in Haskell

---
+++ Problem 24/main.hs
@@ -0,0 +1,12 @@
+import Data.List
+
+main = print $ nthPermutation (1000000 - 1) ['0'..'9']
+
+nthPermutation 0 a = a
+nthPermutation n a = a !! i : nthPermutation j (remove i a) where
+    (i, j) = divMod n (factorial $ length a - 1)
+
+remove i as = l ++ tail r where
+    (l, r) = splitAt i as
+
+factorial n = product [1..n]
