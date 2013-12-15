5f1e1b6451c04dae91dd58760b137ee9a70c734c
Chris Wendt <chrismwendt@gmail.com>
2013-12-14 19:10:27 -0500

    Solve Problem 23 in Haskell

---
+++ Problem 23/main.hs
@@ -0,0 +1,20 @@
+import Data.Numbers.Primes
+import Data.List
+import qualified Data.IntSet as I
+
+main = print $ sum $ filter (\i -> not . any (\a -> I.member (i - a) set) $ takeWhile (<= i `div` 2) as) [1..cap] where
+    set = I.fromList as
+    as = takeWhile (<= cap) abundants
+    cap = 28123
+
+abundants = filter isAbundant [1..]
+
+isAbundant n = sum (properDivisors n) > n
+
+properDivisors n = filter (/= n) $ divisors n
+
+divisors 0 = []
+divisors n = map product . mapM (\(p, v) -> map (p ^) [0..v]) $ primeFactorization n
+
+primeFactorization n = zip (nub l) (map length $ group l) where
+    l = if n == 0 then [] else primeFactors n
