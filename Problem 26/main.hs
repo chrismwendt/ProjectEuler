cafa39f90d1999dafe4644aa160ad0ec5385b62c
Chris Wendt <chrismwendt@gmail.com>
2013-12-17 00:40:50 -0500

    Do not store the unused quotient in the list of remainders

--- Problem 26/main.hs
+++ Problem 26/main.hs
@@ -1,11 +1,11 @@
 main = print $ fst $ maximumBy snd $ zip [1..] $ map (cycleLength 1) [1..999]
 
 cycleLength n d = cycleLength' n d []
-cycleLength' n d m
+cycleLength' n d rs
     | r == 0 = 0
-    | q == 0 = cycleLength' (10 * n) d ((q, r): m)
-    | any ((== r) . snd) m = length (takeWhile ((/= r) . snd) m) + 1
-    | otherwise = cycleLength' r d ((q, r) : m) where
+    | q == 0 = cycleLength' (10 * n) d (r: rs)
+    | any (== r) rs = length (takeWhile (/= r) rs) + 1
+    | otherwise = cycleLength' r d (r : rs) where
     (q, r) = quotRem n d
 
 maximumBy f = foldr1 (\a b -> if compare (f a) (f b) == GT then a else b)
