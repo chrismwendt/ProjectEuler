dbe6b75e4b1ec013e4bfaa83eba18c2786610e71
Chris Wendt <chrismwendt@gmail.com>
2013-12-17 00:26:56 -0500

    Return length of recurring cycle instead of the cycle itself in Problem 26 Haskell

--- Problem 26/main.hs
+++ Problem 26/main.hs
@@ -1,11 +1,11 @@
-main = print $ fst $ maximumBy (length . snd) $ zip [1..] $ map (recurringCycle 1) [1..(1000 - 1)]
+main = print $ fst $ maximumBy snd $ zip [1..] $ map (cycleLength 1) [1..(1000 - 1)]
 
-recurringCycle n d = recurringCycle' n d []
-recurringCycle' n d m
-    | r == 0 = []
-    | q == 0 = recurringCycle' (10 * n) d ((q, r): m)
-    | any ((== r) . snd) m = map fst ((q, r) : (reverse $ takeWhile ((/= r) . snd) m))
-    | otherwise = recurringCycle' r d ((q, r) : m) where
+cycleLength n d = cycleLength' n d []
+cycleLength' n d m
+    | r == 0 = 0
+    | q == 0 = cycleLength' (10 * n) d ((q, r): m)
+    | any ((== r) . snd) m = length (takeWhile ((/= r) . snd) m) + 1
+    | otherwise = cycleLength' r d ((q, r) : m) where
     (q, r) = quotRem n d
 
 maximumBy f = foldr1 (\a b -> if compare (f a) (f b) == GT then a else b)
