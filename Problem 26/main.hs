3f8ba4ee5c6fc40d62424022cb173b8eb5b59019
Chris Wendt <chrismwendt@gmail.com>
2013-12-16 23:51:00 -0500

    Solve Problem 26 in Haskell

---
+++ Problem 26/main.hs
@@ -0,0 +1,13 @@
+import Data.Function
+
+main = print $ fst $ maximumBy (length . snd) $ zip [1..] $ map (recurringCycle 1) [1..(1000 - 1)]
+
+recurringCycle n d = recurringCycle' n d []
+recurringCycle' n d m
+    | r == 0 = []
+    | q == 0 = recurringCycle' (10 * n) d ((q, r): m)
+    | any ((== r) . snd) m = map fst ((q, r) : (reverse $ takeWhile ((/= r) . snd) m))
+    | otherwise = recurringCycle' r d ((q, r) : m) where
+    (q, r) = quotRem n d
+
+maximumBy f = foldr1 (\a b -> if compare (f a) (f b) == GT then a else b)
