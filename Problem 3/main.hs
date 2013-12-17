ffde4cb3fbe58878e10b9b5431510a76d93af154
Chris Wendt <chrismwendt@gmail.com>
2013-12-17 01:11:32 -0500

    Remove redundant parentheses

--- Problem 3/main.hs
+++ Problem 3/main.hs
@@ -4,6 +4,6 @@ factors n = [i | i <- [1..n], isFactor i n]
 
 primeFactors 1 = []
 primeFactors n = p : primeFactors (n `div` p) where
-    p = (factors n) !! 1
+    p = factors n !! 1
 
 main = print $ maximum $ primeFactors 600851475143
