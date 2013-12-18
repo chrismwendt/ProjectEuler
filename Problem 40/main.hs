6bddeb0984de7f8f109efcea188303c3e2be3d34
Chris Wendt <chrismwendt@gmail.com>
2013-12-18 02:56:20 -0500

    Add comments to Problem 40 Haskell

--- Problem 40/main.hs
+++ Problem 40/main.hs
@@ -2,9 +2,12 @@ import Data.Char
 
 main = print $ product $ map d (map (10^) [0..6])
 
+-- begin the search in the span of digits for which the positive integers have 1 digit
 d i = d' (1, i - 1)
+-- repeatedly subtract the number of digits in spans of positive integers with length l until the requested index is contained in this span, then return the digit from the correct integer
 d' (l, i)
     | i <= l * c = let (d, m) = i `divMod` l in digitToInt $ show (10^(l - 1) + d) !! m
     | otherwise = d' (l + 1, i - l * c)
     where
+    -- the number of positive integers with l digits (9, 90, 900, ...)
     c = (10^l - 10^(l - 1))
