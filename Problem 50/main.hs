adb12fada9b2bab361a841c50765a74b20dfe168
Chris Wendt <chrismwendt@gmail.com>
2014-01-17 15:44:04 -0700

    Reorganize code

--- Problem 50/main.hs
+++ Problem 50/main.hs
@@ -9,10 +9,12 @@ main = print $ maxConsecutives 1000000
 
 upper n = length $ takeWhile (<= n) $ scanl1 (+) primes
 
-maxConsecutives n = head $ filter isPrime $ concatMap (takeWhile (<= n') . gen) $ reverse [1 .. upper n']
+maxConsecutives limit = head $ filter isPrime $ concatMap (gen n) $ reverse [1 .. upper n]
     where
-    n' = last $ takeWhile (<= n) primes
+    n = last $ takeWhile (<= limit) primes
 
-gen n = gen' (Seq.fromList $ take n primes) (drop n primes)
+gen n i = takeWhile (<= n) $ gen' (Seq.fromList $ take i primes) (drop i primes)
 
-gen' line ps = Foldable.sum line : gen' (Seq.drop 1 $ line Seq.|> head ps) (tail ps)
+gen' line ps = Foldable.sum line : gen' (shift line (head ps)) (tail ps)
+
+shift s a = Seq.drop 1 $ s Seq.|> a
