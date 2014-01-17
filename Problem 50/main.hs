ab084dd2180c41e496abb88a7a458ae7701382ff
Chris Wendt <chrismwendt@gmail.com>
2014-01-17 13:12:23 -0700

    Use splitAt instead of list difference

--- Problem 50/main.hs
+++ Problem 50/main.hs
@@ -12,9 +12,9 @@ upper n = length $ takeWhile (<= n) $ scanl1 (+) primes
 
 -- Seq from upper and primes list, repeatedly drop from sequence and append from primes list to end of seq
 -- maxConsecutives' n = 3
-maxConsecutives' n = maximum $ (:) 0 $ map length $ go n (sum terp) (Seq.fromList terp) (primes \\ terp)
+maxConsecutives' n = maximum $ (:) 0 $ map length $ go n (sum terp1) (Seq.fromList terp1) terp2
     where
-    terp = take (upper n) primes
+    (terp1, terp2) = splitAt (upper n) primes
 
 go n s acc ps
     -- | null ps = []
