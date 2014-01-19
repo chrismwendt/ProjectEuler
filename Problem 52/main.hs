bac00119dac871635466454dc50a708ee7a650b1
Chris Wendt <chrismwendt@gmail.com>
2014-01-18 22:44:16 -0700

    Solve Problem 52 in Haskell

---
+++ Problem 52/main.hs
@@ -0,0 +1,7 @@
+import Data.List
+import Data.Maybe
+
+main = print $ fromJust $ find (\x -> all (\m -> isMatch x m) $ multiples x) [1..]
+    where
+    isMatch a b = sort (show a) == sort (show b)
+    multiples x = map (x *) [2..6]
