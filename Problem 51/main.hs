d90cfb7692d6e221a0b655dd4450e2a095e26af1
Chris Wendt <chrismwendt@gmail.com>
2014-01-18 17:25:34 -0700

    Revert to older algorithm

--- Problem 51/main.hs
+++ Problem 51/main.hs
@@ -3,34 +3,17 @@ import Data.Maybe
 import Data.List
 import Data.Function
 
-main :: IO ()
-main = print $ head $ solutions 8
+main = print $ fromJust $ find ((8 ==) . length . longestPrimeFamily) primes
 
-solutions :: Integer -> [Integer]
-solutions n = map (\(mask, f) -> head (maskedPrimes mask)) $
-    filter (\(mask, f) -> genericLength f == n && not (null $ maskedPrimes mask)) primeFamilies
+longestPrimeFamily p = maximumBy (compare `on` length) $ map (filter isPrime) $ families p
 
-maskedPrimes :: String -> [Integer]
-maskedPrimes mask = filter isPrime $ map read $ filter noLeadingZero $ sequence [if c == '*' then ['0'..'9'] else [c] | c <- mask]
+families :: Integer -> [[Integer]]
+families p = map (map read) $ map (family $ show p) (masks $ length $ show p)
 
-primeFamilies :: [(String, [Integer])]
-primeFamilies = map (\f -> (fst f, filter isPrime $ snd f)) families
+family p m = filter (not . isPrefixOf "0") $ map (replace (apply m p) '*') ['0'..'9']
 
-families :: [(String, [Integer])]
-families = map family language
-    where
-    family s = (s, family' s)
-    family' s = map read $ filter noLeadingZero $ map (replace s '*') ['0'..'9']
+masks n = filter (replicate n ' ' /=) $ sequence (replicate n "* ")
 
-language :: [String]
-language = filter noLeadingZero language'
-    where
-    language' = concatMap stringsN [1..]
-    stringsN n = filter (elem '*') (stringsN' n)
-    stringsN' n = sequence (replicate n ('*' : ['0'..'9']))
+apply mask s = [if l == '*' then '*' else r | (l, r) <- zip mask s]
 
-replace :: String -> Char -> Char -> String
 replace s o n = [if c == o then n else c | c <- s]
-
-noLeadingZero :: String -> Bool
-noLeadingZero = not . isPrefixOf "0"
