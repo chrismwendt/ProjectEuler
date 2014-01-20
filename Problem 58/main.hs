edc6f1676cbb1ce24802ff8edfdd6a7ae2d5e2b0
Chris Wendt <chrismwendt@gmail.com>
2014-01-20 01:31:13 -0700

    Pass the number of steps along the edge through

--- Problem 58/main.hs
+++ Problem 58/main.hs
@@ -4,16 +4,17 @@ import Data.Ratio
 import Data.Maybe
 
 main :: IO ()
-main = print $ (\(a, b, c) -> (head c !! 1) - (head c !! 0) - 1) $ fromJust $ find condition $ dropWhile (\(a, b, c) -> a == 0 || b == 0) $ iterate step (0, 0, rings)
+main = print $ (\(a, b, ((l, s):ls)) -> s - 1) $ fromJust $ find condition $ dropWhile (\(a, b, c) -> a == 0 || b == 0) $ iterate step (0, 0, rings)
 
 condition (np, nc, l) = fromInteger np / fromInteger (np + nc) < 0.1
 
-step (np, nc, (l:ls)) = (np + genericLength np', nc + genericLength nc', ls)
+step :: (Integer, Integer, [([Integer], Integer)]) -> (Integer, Integer, [([Integer], Integer)])
+step (np, nc, ((l, s):ls)) = (np + genericLength np', nc + genericLength nc', ls)
     where
     (np', nc') = partition isPrime l
 
-rings :: [[Integer]]
-rings = map fst $ iterate step ([1], 0)
+rings :: [([Integer], Integer)]
+rings = iterate step ([1], 0)
     where
     step = (\(l, s) -> (corners (last l) (s + 2), s + 2))
 
