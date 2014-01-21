79d64163393193b774eae5fbeb4966630ef84c8b
Chris Wendt <chrismwendt@gmail.com>
2014-01-20 23:57:43 -0700

    Transfer limit of denominations by value to a filter

--- Problem 31/main.hs
+++ Problem 31/main.hs
@@ -5,5 +5,4 @@ main = print $ ways [1, 2, 5, 10, 20, 50, 100, 200] 200
 ways _ 0 = 1
 ways denominations value = sum $
     [ways (filter (<= d) denominations) (value - d) |
-        d <- denominations,
-        d <= value]
+        d <- filter (<= value) denominations]
