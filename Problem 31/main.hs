035598d3b48dfb515129ea036c491f08ae31b6f0
Chris Wendt <chrismwendt@gmail.com>
2014-01-20 23:55:16 -0700

    Remove the redundant function

--- Problem 31/main.hs
+++ Problem 31/main.hs
@@ -2,10 +2,8 @@ import Data.List
 
 main = print $ ways [1, 2, 5, 10, 20, 50, 100, 200] 200
 
-ways denominations value = ways' denominations value
-
-ways' _ 0 = 1
-ways' denominations value = sum $
-    [ways' (filter (<= d) denominations) (value - d) |
+ways _ 0 = 1
+ways denominations value = sum $
+    [ways (filter (<= d) denominations) (value - d) |
         d <- denominations,
         d <= value]
