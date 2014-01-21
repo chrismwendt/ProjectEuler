2f8a385a207d6a1cc560a74e417d4d6c56e61cef
Chris Wendt <chrismwendt@gmail.com>
2014-01-21 10:46:04 -0500

    Reduce repetition by parameterization and recursion

--- Problem 60/main.hs
+++ Problem 60/main.hs
@@ -3,15 +3,19 @@ import Data.Maybe
 import Data.List
 
 main :: IO ()
-main = print $ head
-    [[a, b, c, d, e] |
-        a <- primes,
-        b <- filter (\p -> concatable p [a]) $ takeWhile (< a) primes,
-        c <- filter (\p -> concatable p [a, b]) $ takeWhile (< b) primes,
-        d <- filter (\p -> concatable p [a, b, c]) $ takeWhile (< c) primes,
-        e <- filter (\p -> concatable p [a, b, c, d]) $ takeWhile (< d) primes]
+main = print $ sum $ head $ concatableSetsN 5
 
-concatable :: Integer -> [Integer] -> Bool
-concatable i ps = and [isPrime (read $ i' ++ p') && isPrime (read $ p' ++ i') |
+concatableSetsN :: Integer -> [[Integer]]
+concatableSetsN n = concatMap (\p -> map ((:) p) $ concatableSetsN' [p] (n - 1)) primes
+
+concatableSetsN' :: [Integer] -> Integer -> [[Integer]]
+concatableSetsN' prevs n
+    | n == 0 = [[]]
+    | otherwise = [p : set |
+        p <- filter (concatable prevs) $ takeWhile (< head prevs) primes,
+        set <- concatableSetsN' (p:prevs) (n - 1)]
+
+concatable :: [Integer] -> Integer -> Bool
+concatable ps i = and [isPrime (read $ i' ++ p') && isPrime (read $ p' ++ i') |
     p <- ps,
     let (i', p') = (show i, show p)]
