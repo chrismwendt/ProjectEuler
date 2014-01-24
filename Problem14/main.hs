fc12ff9aed3069fd3af363c20a4a0aa082149474
Chris Wendt <chrismwendt@gmail.com>
2014-01-23 22:23:15 -0700

    Qualify IntMap as M

--- Problem14/main.hs
+++ Problem14/main.hs
@@ -2,7 +2,7 @@
 -- TODO increase speed
 
 import Data.List
-import qualified Data.IntMap as I
+import qualified Data.IntMap as M
 import Data.Maybe
 
 main :: IO ()
@@ -10,17 +10,17 @@ main = print . (+) 1 . fromJust $ elemIndex (maximum l) l where
     l = collatzLengths [1..1000000]
 
 collatzLengths :: [Int] -> [Int]
-collatzLengths = collatzLengths' (I.singleton 1 1)
+collatzLengths = collatzLengths' (M.singleton 1 1)
 
-collatzLengths' :: I.IntMap Int -> [Int] -> [Int]
+collatzLengths' :: M.IntMap Int -> [Int] -> [Int]
 collatzLengths' _ [] = []
 collatzLengths' m (x:xs) = v : collatzLengths' m' xs where
     (v, m') = insertCollatz x m
 
-insertCollatz :: Int -> I.IntMap Int -> (Int, I.IntMap Int)
-insertCollatz x m = case I.lookup x m of
+insertCollatz :: Int -> M.IntMap Int -> (Int, M.IntMap Int)
+insertCollatz x m = case M.lookup x m of
     Just v -> (v, m)
-    Nothing -> (v' + 1, I.insert x (v' + 1) m') where
+    Nothing -> (v' + 1, M.insert x (v' + 1) m') where
         (v', m') = insertCollatz (step x) m
 
 step :: Int -> Int
