9d916fafe977f13da94ec1888e693db691093864
Chris Wendt <chrismwendt@gmail.com>
2014-01-31 08:37:54 -0500

    Inline overlaps check

--- Problem61/main.hs
+++ Problem61/main.hs
@@ -6,15 +6,12 @@ main = print $ sum $
     fromJust $ find (\l -> overlap2 (last l) (head l)) $ concatMap (sequenceF f) $
     cyclicPermutations $ map (takeWhile (< 10000) . dropWhile (< 1000)) polygons
     where
-    f x = filter (\l -> overlaps (x:take 1 l))
+    f x = filter (\l -> null l || overlap2 x (head l))
 
 sequenceF :: Monad m => (a -> m [a] -> m [a]) -> [m a] -> m [a]
 sequenceF f = foldr mfcons (return [])
     where
     mfcons p q = p >>= \x -> f x q >>= \y -> return (x:y)
-
-overlaps :: [Int] -> Bool
-overlaps as = all (uncurry overlap2) $ zip as (tail as)
 
 overlap2 :: Int -> Int -> Bool
 overlap2 a b = a `mod` 100 == b `div` 100
