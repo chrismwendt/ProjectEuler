91c850bb09062a86d53adca815d4f6b30f73609c
Chris Wendt <chrismwendt@gmail.com>
2014-02-01 22:55:45 -0500

    Use Integer because the result is very large (just happened to fit in 64-bit Int)

--- Problem62/main.hs
+++ Problem62/main.hs
@@ -6,11 +6,11 @@ import Control.Monad.State as S
 main :: IO ()
 main = print $ minimum $ fromJust $ find ((5 ==) . length) $ S.evalState (mapM collisions cubes) M.empty
 
-collisions :: Int -> S.State (M.Map String [Int]) [Int]
+collisions :: Integer -> S.State (M.Map String [Integer]) [Integer]
 collisions n = let n' = sort $ show n in do
     modify $ M.insertWith (++) n' [n]
     s <- S.get
     return $ M.findWithDefault [] n' s
 
-cubes :: [Int]
+cubes :: [Integer]
 cubes = map (^3) [1..]
