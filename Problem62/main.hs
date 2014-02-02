a9360c17f51c94e0bc7f7184c1c6dc1858a66de6
Chris Wendt <chrismwendt@gmail.com>
2014-02-02 03:06:26 -0500

    Group state into a tuple

--- Problem62/main.hs
+++ Problem62/main.hs
@@ -2,18 +2,15 @@ import Data.List
 import qualified Data.Map as M
 
 main :: IO ()
-main = print $ minimum . fst3 $ until found next ([], M.empty, cubes)
+main = print $ minimum . fst $ until found next ([], (M.empty, cubes))
 
-fst3 :: (a, b, c) -> a
-fst3 (a, _, _) = a
+found ::  ([a], (b, c)) -> Bool
+found = (5 ==) . length . fst
 
-found ::  ([a], b, c) -> Bool
-found = (5 ==) . length . fst3
-
-type State = ([Integer], M.Map String [Integer], [Integer])
+type State = ([Integer], (M.Map String [Integer], [Integer]))
 
 next :: State -> State
-next (_, m, c:cs) = (cur, M.insert c' cur m, cs)
+next (_, (m, c:cs)) = (cur, (M.insert c' cur m, cs))
     where
     c' = sort $ show c
     cur = c : M.findWithDefault [] c' m
