c8b1d2b64dd800f7d101072151e4422068792a4e
Chris Wendt <chrismwendt@gmail.com>
2014-02-02 22:04:09 -0500

    Switch back to state monad, but with iterateUntil and MultiMap

--- Problem62/main.hs
+++ Problem62/main.hs
@@ -1,19 +1,18 @@
 import Data.List
 import qualified Data.MultiMap as M
+import qualified Control.Monad.Loops as L
+import qualified Control.Monad.State as S
 
 main :: IO ()
-main = print $ minimum . fst $ until found next ([], (M.empty, cubes))
+main = print $ minimum $ S.evalState (L.iterateUntil ((5 ==) . length) collisions) (M.empty, cubes)
 
-found ::  ([a], (b, c)) -> Bool
-found = (5 ==) . length . fst
-
-type State = ([Integer], (M.MultiMap String Integer, [Integer]))
-
-next :: State -> State
-next (_, (m, c:cs)) = (M.lookup c' m', (m', cs))
-    where
-    c' = sort $ show c
-    m' = M.insert c' c m
+collisions :: S.State (M.MultiMap String Integer, [Integer]) [Integer]
+collisions = do
+    (mmap, n:ns) <- S.get
+    let k = sort $ show n
+    S.put (M.insert k n mmap, ns)
+    (mmap', _) <- S.get
+    return $ M.lookup k mmap'
 
 cubes :: [Integer]
 cubes = map (^3) [1..]
