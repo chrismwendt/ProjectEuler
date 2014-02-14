99bfa061f6b7c0e6ab6189474a1310ad25d2e248
Chris Wendt <chrismwendt@gmail.com>
2014-02-14 18:25:47 -0500

    Speed up Integer concatenation

--- Problem60/main.hs
+++ Problem60/main.hs
@@ -1,6 +1,7 @@
 import Data.Numbers.Primes
 import Data.Maybe
 import Data.List
+import Math.NumberTheory.Logarithms
 
 -- TODO try using a concatable lookup table with the state monad
 
@@ -17,4 +18,7 @@ setsN' prevs n
         p <- filter (concatable prevs) $ takeWhile (< head prevs) primes]
 
 concatable :: [Integer] -> Integer -> Bool
-concatable ps i = and [isPrime $ read $ f (++) (show i) (show p) | p <- ps, f <- [id, flip]]
+concatable ps i = and [isPrime $ f concatenate i p | p <- ps, f <- [id, flip]]
+
+concatenate :: Integer -> Integer -> Integer
+concatenate a b = a * 10^(integerLogBase 10 b + 1) + b
