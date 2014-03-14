6ade2a88a96fd23466c80c09149088caf38e8743
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 08:48:14 -0400

    Make palindrome more generic

--- Problem55/main.hs
+++ Problem55/main.hs
@@ -2,10 +2,10 @@ main :: IO ()
 main = print $ length $ filter isLychrel [0 .. 10^4 - 1]
 
 isLychrel :: Integer -> Bool
-isLychrel n = not $ any palindrome $ (if n < 10^4 then take 50 else id) $ tail $ iterate step n
+isLychrel n = not $ any (palindrome . show) $ (if n < 10^4 then take 50 else id) $ tail $ iterate step n
 
 step :: Integer -> Integer
 step n = n + read (reverse $ show n)
 
-palindrome :: Integer -> Bool
-palindrome n = show n == reverse (show n)
+palindrome :: Eq a => [a] -> Bool
+palindrome s = s == reverse s
