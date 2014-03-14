0dd2cb021232a24c3066e8015f2d76e7f2f6b6a0
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 19:08:27 -0400

    Prefer '$' over '.'

--- Problem11/main.hs
+++ Problem11/main.hs
@@ -27,7 +27,7 @@ map2D :: (a -> b) -> [[a]] -> [[b]]
 map2D = map . map
 
 grid :: [[Integer]]
-grid = map2D read . map words . lines $
+grid = map2D read $ map words $ lines $
     "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n\
     \49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00\n\
     \81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65\n\
