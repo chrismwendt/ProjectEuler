c261b10369459f46c56700d8664bf7a3f8d8ef3c
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 13:44:28 -0400

    Reimplement descendingProducts using a heap

--- Problem4/main.hs
+++ Problem4/main.hs
@@ -1,24 +1,22 @@
 import Data.List
 import Data.Maybe
+import qualified Data.Heap as H
+
+data Node = Node Integer [Node] deriving (Eq, Ord, Show)
 
 main :: IO ()
-main = print $ fromJust $ find (palindrome . show) $ descendingProducts ns ns where
-    ns = [999, 998 .. 100]
-
-descendingProducts :: (Num a, Ord a) => [a] -> [a] -> [a]
-descendingProducts [] _ = []
-descendingProducts _ [] = []
-descendingProducts (n:ns) (m:ms) = n * m : mergeBy descending column rest where
-    descending = flip compare
-    column = map (* n) ms
-    rest = descendingProducts ns ms
+main = print $ fromJust $ find (palindrome . show) $ descendingProducts 999 100
 
 palindrome :: Eq a => [a] -> Bool
 palindrome s = s == reverse s
 
-mergeBy :: (t -> t -> Ordering) -> [t] -> [t] -> [t]
-mergeBy _ a [] = a
-mergeBy _ [] b = b
-mergeBy cmp (a:as) (b:bs)
-    | cmp a b == LT = a : mergeBy cmp as (b:bs)
-    | otherwise = b : mergeBy cmp (a:as) bs
+descendingProducts :: Integer -> Integer -> [Integer]
+descendingProducts high low = unfoldr (fmap f . H.view) $ (H.singleton $ diagonalNode high low :: H.MaxHeap Node)
+
+f ((Node i ns), t) = (i, H.union t (H.fromList ns))
+
+columnNode :: Integer -> Integer -> Integer -> Node
+columnNode slice high low = Node (slice * high) (if high == low then [] else [columnNode slice (pred high) low])
+
+diagonalNode :: Integer -> Integer -> Node
+diagonalNode high low = Node (high * high) (if high == low then [] else [diagonalNode (pred high) low, columnNode high (pred high) low])
