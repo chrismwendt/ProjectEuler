bc5bfbce0f46992122f303841d0c7dd36b579996
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 14:53:03 -0400

    Reorganize control flow

--- Problem4/main.hs
+++ Problem4/main.hs
@@ -2,21 +2,23 @@ import Data.List
 import Data.Maybe
 import qualified Data.Heap as H
 
-data Node = Node Integer [Node] deriving (Eq, Ord, Show)
+data Node a = Node a [Node a] deriving (Eq, Ord, Show)
 
 main :: IO ()
-main = print $ fromJust $ find (palindrome . show) $ descendingProducts 999 100
+main = print $ fromJust $ find (palindrome . show) $ descendingProducts [999, 998 .. 100]
 
 palindrome :: Eq a => [a] -> Bool
 palindrome s = s == reverse s
 
-descendingProducts :: Integer -> Integer -> [Integer]
-descendingProducts high low = unfoldr (fmap f . H.view) $ (H.singleton $ diagonalNode high low :: H.MaxHeap Node)
+descendingProducts :: [Integer] -> [Integer]
+descendingProducts as = unfoldr (fmap f . H.view) (H.singleton $ diagonalNode as :: H.MaxHeap (Node Integer))
+    where
+    f (Node i ns, t) = (i, t `H.union` H.fromList ns)
 
-f ((Node i ns), t) = (i, H.union t (H.fromList ns))
+columnNode :: [a] -> Node a
+columnNode [a] = Node a []
+columnNode (a:as) = Node a [columnNode as]
 
-columnNode :: Integer -> Integer -> Integer -> Node
-columnNode slice high low = Node (slice * high) (if high == low then [] else [columnNode slice (pred high) low])
-
-diagonalNode :: Integer -> Integer -> Node
-diagonalNode high low = Node (high * high) (if high == low then [] else [diagonalNode (pred high) low, columnNode high (pred high) low])
+diagonalNode :: [Integer] -> Node Integer
+diagonalNode [a] = Node (a * a) []
+diagonalNode (a:as) = Node (a * a) [diagonalNode as, columnNode $ map (* a) as]
