ecd0bb8de0fa67c02f344aa7183c06741cb6992d
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 16:49:53 -0400

    Use brute force since the search space is <1m

--- Problem4/main.hs
+++ Problem4/main.hs
@@ -1,29 +1,5 @@
-{-# LANGUAGE FlexibleInstances #-}
-
-import Data.List
-import Data.Maybe
-import qualified Data.Heap as H
-import qualified Data.Tree as T
-import Data.Ord
-
-instance Ord a => Ord (T.Tree a) where
-    compare = comparing T.rootLabel
-
 main :: IO ()
-main = print $ fromJust $ find (palindrome . show) $ descendingProducts [999, 998 .. 100]
+main = print $ maximum $ filter (palindrome . show) $ [n*m | n <- [100..999], m <- [100..999]]
 
 palindrome :: Eq a => [a] -> Bool
 palindrome s = s == reverse s
-
-descendingProducts :: [Integer] -> [Integer]
-descendingProducts as = unfoldr (fmap f . H.view) (H.singleton $ diagonal as :: H.MaxHeap (T.Tree Integer))
-    where
-    f (T.Node i ns, t) = (i, t `H.union` H.fromList ns)
-
-column :: [a] -> T.Tree a
-column [a] = T.Node a []
-column (a:as) = T.Node a [column as]
-
-diagonal :: [Integer] -> T.Tree Integer
-diagonal [a] = T.Node (a * a) []
-diagonal (a:as) = T.Node (a * a) [diagonal as, column $ map (* a) as]
