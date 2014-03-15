f16fda15684b22c2f278823f87749adc35dc8227
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 20:12:29 -0400

    Rename isCircular to isCyclicPrime to be more intuitive

--- Problem35/main.hs
+++ Problem35/main.hs
@@ -1,10 +1,10 @@
 import Data.Numbers.Primes
 
 main :: IO ()
-main = print $ length $ filter isCircular $ takeWhile (< 1000000) primes
+main = print $ length $ filter isCyclicPrime $ takeWhile (< 1000000) primes
 
-isCircular :: Show a => a -> Bool
-isCircular n = all isPrime $ map (\i -> read $ take l $ drop i $ cycle s) [1 .. l]
+isCyclicPrime :: Show a => a -> Bool
+isCyclicPrime n = all isPrime $ map (\i -> read $ take l $ drop i $ cycle s) [1 .. l]
     where
     l = length s
     s = show n
