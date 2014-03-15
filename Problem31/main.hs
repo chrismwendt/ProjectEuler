a6ce7213597360ea7e3971db5c65aa38295d7ed3
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 20:07:27 -0400

    Redundant $

--- Problem31/main.hs
+++ Problem31/main.hs
@@ -5,6 +5,6 @@ main = print $ ways [1, 2, 5, 10, 20, 50, 100, 200] 200
 
 ways :: [Integer] -> Integer -> Integer
 ways _ 0 = 1
-ways denominations value = sum $
+ways denominations value = sum
     [ways (filter (<= d) denominations) (value - d) |
         d <- filter (<= value) denominations]
