0cecfc655e363b6b30d669f8cc2e6bb6e9c148aa
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 20:07:14 -0400

    Make type more obvious

--- Problem31/main.hs
+++ Problem31/main.hs
@@ -3,7 +3,7 @@ import Data.List
 main :: IO ()
 main = print $ ways [1, 2, 5, 10, 20, 50, 100, 200] 200
 
-ways :: (Num t, Num a, Ord t) => [t] -> t -> a
+ways :: [Integer] -> Integer -> Integer
 ways _ 0 = 1
 ways denominations value = sum $
     [ways (filter (<= d) denominations) (value - d) |
