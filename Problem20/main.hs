5f7ef01725181ae05dfa941f87bc6be0ea411d33
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 20:02:41 -0400

    Remove redundant $

--- Problem20/main.hs
+++ Problem20/main.hs
@@ -1,7 +1,7 @@
 import Data.Char
 
 main :: IO ()
-main = print $ sum $ map digitToInt $ show $ factorial $ 100
+main = print $ sum $ map digitToInt $ show $ factorial 100
 
 factorial :: Integer -> Integer
 factorial n = product [1 .. n]
