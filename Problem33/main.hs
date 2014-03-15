d525873265d9fb6d59638642e34a677abf78f27b
Chris Wendt <chrismwendt@gmail.com>
2014-03-14 20:08:30 -0400

    Make type more obvious

--- Problem33/main.hs
+++ Problem33/main.hs
@@ -4,7 +4,7 @@ import Data.List
 main :: IO ()
 main = print $ denominator $ product $ map (uncurry (%)) $ filter isNontrivial $ [(n, d) | n <- [10 .. 99], d <- [n + 1 .. 99]]
 
-isNontrivial :: (Integral a, Read a, Show a) => (a, a) -> Bool
+isNontrivial :: (Integer, Integer) -> Bool
 isNontrivial (n, d)
     | '0' `elem` (sn ++ sd) = False
     | otherwise = any (\di -> (remove di sn) % (remove di sd) == n % d) $ sn ++ sd
