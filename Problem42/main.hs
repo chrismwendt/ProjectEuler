45361ff1e7ffb15424312bbbf93d19b121c2d684
Chris Wendt <chrismwendt@gmail.com>
2014-03-31 05:04:26 -0400

    Use splitOn from the split package

--- Problem42/main.hs
+++ Problem42/main.hs
@@ -1,5 +1,5 @@
 import System.IO
-import qualified Data.Text as T
+import Data.List.Split
 import Data.Char
 import Data.List
 import qualified Data.Set as S
@@ -8,9 +8,6 @@ main :: IO ()
 main = do
     text <- readFile "words.txt"
     print $ length $ filter isTriangleWord $ splitOn "," $ filter (/= '"') $ text
-
-splitOn :: String -> String -> [String]
-splitOn s = map T.unpack . T.splitOn (T.pack s) . T.pack
 
 isTriangleWord :: [Char] -> Bool
 isTriangleWord n = S.member (worth n) triangles26
